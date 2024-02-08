17cc86b998419a9b438a4f91c447e122ae767611
andres arriaga <andresfelipearriaga@gmail.com>
2024-02-07 22:36:12 -0500

    cambiando proyecto  de portafolio a tienda virtual

--- src/components/proyectos.jsx
+++ src/components/proyectos.jsx
@@ -1,5 +1,4 @@
 import Cine from '../assets/miCine.png'
-import PrimerPortafolio from '../assets/primerPortafolio.png'
 import Tailwin from '../assets/tailwind.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCss3Alt, faHtml5, faJs, faNodeJs, faReact } from '@fortawesome/free-brands-svg-icons';
@@ -21,12 +20,12 @@ export const Proyectos = () => {
         {/* Card primer portafolio*/}
       <div className="p-4 lg:w-2/5 border-2  border-[#d2d5da] dark:border-[#26304b] rounded-lg m-1 hover:scale-105 transition ease-in-out duration-300 ">
         <div className="h-full flex gap-3 sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
-        <a href="https://portafolioarriaga.vercel.app/" className='flex-shrink-0  w-48 h-48 sm:mb-0 mb-4 cursor-pointer'>
-                <img alt="team" className="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4" src={PrimerPortafolio} />
+        <a href="https://custom-gaming.vercel.app/" className='flex-shrink-0  w-48 h-48 sm:mb-0 mb-4 cursor-pointer'>
+                <img alt="team" className="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4" src='https://vercel.com/_next/image?url=%2Fapi%2Fscreenshot%3Fdark%3D1%26deploymentId%3Ddpl_Cf6yNoYFvLz4Yw8afTp2uxevjxZY%26teamId%3Dandres-arriagas-projects%26withStatus%3D1&w=640&q=75&dpl=dpl_F1GWNVJiC5Mzy74zbtZ8dfQ1zFNS' />
         </a>
           <div className="flex-grow ">
-            <h2 className="title-font fontMedium text-lg text-gray-900 font-bold dark:text-[#eaebf1] pb-2">Primer Portafolio</h2>
-            <p className="mb-4 dark:text-[#eaebf1]">Es el primer portafolio que decidi a realizar para aventarme a este mundo de la programacion.
+            <h2 className="title-font fontMedium text-lg text-gray-900 font-bold dark:text-[#eaebf1] pb-2">Tienda online</h2>
+            <p className="mb-4 dark:text-[#eaebf1]">carrito de compra, formulario, loguin, proceso de mySql y api, tienda basica  .
             <br />
             <br />
             </p>
@@ -34,7 +33,10 @@ export const Proyectos = () => {
                 {/* iconos tecnologia */}
                 <FontAwesomeIcon icon={faJs} className='text-[#f8d65b] w-5 h-5 pr-2' />
                 <FontAwesomeIcon icon={faHtml5} className='text-[#e06e3c] w-5 h-5 pr-2' />
-                <FontAwesomeIcon icon={faCss3Alt} className='text-[#4994ec] w-5 h-5' />
+                <FontAwesomeIcon icon={faCss3Alt} className='text-[#4994ec] w-5 h-5 pr-2' />
+                <FontAwesomeIcon icon={faReact} className='text-[#93e8f5] w-5 h-5 pr-2'/>
+                <FontAwesomeIcon icon={faNodeJs} className='text-[#97c33c] w-5 h-5 pr-2'/>
+                <img src={Tailwin} alt="tailwind css" className='w-5 h-4 pt-1'/>
             </span>
           </div>
         </div>
